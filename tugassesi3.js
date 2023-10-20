30863f0e141c66554756ec831aca183583e2f287
Rustiani1 <123487139+Rustiani1@users.noreply.github.com>
2023-10-20 21:56:25 +0700

    Add files via upload

---
+++ tugassesi3.js
@@ -0,0 +1,25 @@
+// Variabel dengan tipe object
+var restaurantObject = {
+    name: "The Vegan Oasis",
+    city: "Sukabumi",
+    favoriteDrink: "Strawberry Smoothie",
+    favoriteFood: "Vegan Sushi Rolls",
+    isVegan: "true"
+};
+console.table(restaurantObject);
+
+ /*
+ Variabel dengan tipe array
+ menggunakan kurung kotak sebagai penanda array
+ dan kurung kurawal sebagai pengisi value array
+ */
+var restaurantArray = [
+    {
+        name: "The Vegan Oasis",
+        city: "Sukabumi",
+        favoriteDrink: "Strawberry Smoothie",
+        favoriteFood: "Vegan Sushi Rolls",
+        isVegan: true
+    }
+];
+console.log(restaurantObject)
